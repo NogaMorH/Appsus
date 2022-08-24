f99c93097ecbedf87db5cf030086adffd32e6ff8
Noga Mor-Havilio <nogamorh@gmail.com>
2022-08-24 20:36:55 +0300

    set time for display mail

--- apps/mail/cmps/mail-preview.jsx
+++ apps/mail/cmps/mail-preview.jsx
@@ -1,10 +1,22 @@
 export function MailPreview({ mail }) {
-    const { subject, from, body } = mail
+    const { subject, from, body, sentAt } = mail
     const { senderName } = from
+
+    setTimeForDisplay()
+
+    function setTimeForDisplay() {
+        const date = new Date(sentAt)
+        const year = date.getFullYear()
+        const month = date.getMonth() + 1
+        const day = date.getDate()
+        return `${day}/${month}/${year}`
+    }
+
     return <li className="mail-preview">
         <span>icons</span>
-        <span>{senderName}</span>
-        <span>{subject} - {body}</span>
-        <span>time</span>
+        <span className="hide-long-text">{senderName}</span>
+        <span className="hide-long-text">{subject} - {body}</span>
+        <span className="hide-long-text">{setTimeForDisplay()}</span>
     </li>
 }
+
