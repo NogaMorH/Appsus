5a4d8f4ac0d265cd418abfc5e5371684ee01387e
kfirazu <kfirazu@gmail.com>
2022-08-24 17:09:54 +0300

    renderd note preview

--- apps/note/cmps/note-list.jsx
+++ apps/note/cmps/note-list.jsx
@@ -3,7 +3,7 @@ import { NotePreview } from './note-preview.jsx'
 export function NoteList({ notes }) {
     console.log('notes from note list:', notes)
     return <section className="note-list-container">
-        {notes.map(note => <NotePreview /> )}
-       
+        {notes.map(note => <NotePreview note={note} key={note.id} />)}
+
     </section>
 }
--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -1,5 +1,17 @@
-export function NotePreview() {
+export function NotePreview({ note }) {
+    console.log('note:', note)
+        const{ text } = note.info
     return <article className="note-preview">
-        
+        {text}
     </article>
 }
+
+// {
+    // {
+    //     id: utilService.makeId(),
+    //         type: 'note-text',
+    //             isPinned: true,
+    //                 info: {
+    //         text: 'Fullstack Me Baby!'
+    //     }
+    // },
