b49928cb66ba4839400c3af60ee7ad9268a25f05
kfirazu <kfirazu@gmail.com>
2022-08-24 19:08:47 +0300

    changed keep name to note

--- apps/note/cmps/note-preview.jsx
+++ apps/note/cmps/note-preview.jsx
@@ -5,7 +5,7 @@ export function NotePreview({ note }) {
     console.log('note:', note)
     const { text, url, title } = note.info
     return <article className="note-preview">
-        <Link to={"/keep/" + note.id}>
+        <Link to={"/note/" + note.id}>
             <p>{text}</p>
             <h3>{title}</h3>
             <img src={url} />
