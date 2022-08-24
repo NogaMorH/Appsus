d5d89888774c5b2cce7a2e33f9becd8e92ec3136
Noga Mor-Havilio <nogamorh@gmail.com>
2022-08-24 15:44:31 +0300

    basic mail index

--- apps/mail/views/mail-index.jsx
+++ apps/mail/views/mail-index.jsx
@@ -1,7 +1,22 @@
+import { mailService } from '../services/mail.service.js'
 import { MailList } from "../cmps/mail-list.jsx"
 
 export class MailIndex extends React.Component {
+
+    state = {
+        mails: null
+    }
+
+    componentDidMount() {
+        this.loadMails()
+    }
+
+    loadMails() {
+        mailService.query()
+            .then((mails) => this.setState({ mails }))
+    }
+
     render() {
-        return <MailList />
+        return <MailList mails={mails} />
     }
 }
