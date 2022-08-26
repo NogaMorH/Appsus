a2cc47e32a0ebe9538956877f80e021942c00f57
Noga Mor-Havilio <nogamorh@gmail.com>
2022-08-26 17:53:43 +0300

    style app nav modal amd header

--- cmps/app-nav-modal.jsx
+++ cmps/app-nav-modal.jsx
@@ -1,11 +1,21 @@
 const { Link } = ReactRouterDOM
 
 export function AppNavModal() {
-    return <nav className="app-nav-modal">
-        <Link exact="true" to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/mail">Email</Link>
-        <Link to="/note">Keep</Link>
-        <Link to="/book">Books</Link>
+    return <nav className="flex space-between app-nav-modal">
+        <Link exact="true" to="/" className="app-nav-link">
+            <img src="../assets/img/home.png" className="app-nav-icon" />
+            Home</Link>
+        <Link to="/mail" className="app-nav-link">
+            <img src="../assets/img/mail.png" className="app-nav-icon" />
+            Email</Link>
+        <Link to="/note" className="app-nav-link">
+            <img src="../assets/img/note.png" className="app-nav-icon" />
+            Keep</Link>
+        <Link to="/book" className="app-nav-link">
+            <img src="../assets/img/book.png" className="app-nav-icon" />
+            Books</Link>
+        <Link to="/about" className="app-nav-link">
+            <img src="../assets/img/about.png" className="app-nav-icon" />
+            About</Link>
     </nav>
 }
