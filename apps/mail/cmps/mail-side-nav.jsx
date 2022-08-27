69664a1c642e1a30b9458b777427ff53c7f1e711
Noga Mor-Havilio <nogamorh@gmail.com>
2022-08-28 02:53:49 +0300

    fixed images bug

--- apps/mail/cmps/mail-side-nav.jsx
+++ apps/mail/cmps/mail-side-nav.jsx
@@ -10,7 +10,7 @@ export function MailSideNav({ composeMail, onToggleMenu }) {
     return <aside className="mail-side-nav">
         <button onClick={onCompose} className="btn main-layout compose-btn">Compose</button>
         <NavLink to="/mail" className="flex align-center inbox">
-            <img src="./../assets/img/envelope.png" className="main-layout" />
+            <img src="./assets/img/envelope.png" className="main-layout" />
             Inbox
         </NavLink>
     </aside>
--- apps/note/cmps/note-buttons.jsx
+++ apps/note/cmps/note-buttons.jsx
@@ -22,9 +22,9 @@ export class NoteButtons extends React.Component {
         const { onRemoveNote, note } = this.props
         const { toggleBgColorPallete, onUpdateNoteBgColor } = this
         return <section className="note-buttons">
-            <button onClick={toggleBgColorPallete} className="btn note-btn"><img src="./../../assets/img/color-pallete.png" alt="color-pallete-button" /></button>
-            <button className="btn note-btn"><img src="./../../assets/img/duplicate.png" alt="color-duplicate-button" /></button>
-            <button className="btn note-btn" onClick={() => onRemoveNote(note.id)}><img src="./../../assets/img/trash.png" alt="delete note-button" /></button>
+            <button onClick={toggleBgColorPallete} className="btn note-btn"><img src="./assets/img/color-pallete.png" alt="color-pallete-button" /></button>
+            <button className="btn note-btn"><img src="./assets/img/duplicate.png" alt="color-duplicate-button" /></button>
+            <button className="btn note-btn" onClick={() => onRemoveNote(note.id)}><img src="./assets/img/trash.png" alt="delete note-button" /></button>
             <button className="btn note-btn save">Save </button>
             <React.Fragment>
                 {colorPalleteOpen && <div className="color-btns-container">
